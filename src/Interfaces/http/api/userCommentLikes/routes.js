02dab331d100dc8381798a5ca9fca41386cb9d10
Fauzi0309 <fauzirahmandhani@gmail.com>
2024-12-19 14:22:35 +0700

    scenario usercommentlike for failed test ci

--- src/Interfaces/http/api/userCommentLikes/routes.js
+++ src/Interfaces/http/api/userCommentLikes/routes.js
@@ -1,11 +1,11 @@
 const routes = (handler) => ([
-    {
-      method: 'PUT',
-      path: '/threads/{threadId}/comments/{commentId}/likes',
-      handler: handler.putCommentLikeHandler,
-      options: {
-        auth: 'forumapiapp_jwt'
-      }
-    }
+    // {
+    //   method: 'PUT',
+    //   path: '/threads/{threadId}/comments/{commentId}/likes',
+    //   handler: handler.putCommentLikeHandler,
+    //   options: {
+    //     auth: 'forumapiapp_jwt'
+    //   }
+    // }
   ])
   module.exports = routes
